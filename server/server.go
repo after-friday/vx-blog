9bf09c1870a26cfda0b6b6f079a1f9f7c6c8fdd0
thananan-dev <thananan.math.acs@gmail.com>
2023-08-03 17:38:07 +0700

    :wrench: create configs and connecting to database

--- server/server.go
+++ server/server.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+
 	"github.com/after-friday/vx-blog/controllers/authentication"
 	"github.com/gofiber/fiber/v2"
+	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	initConfig()
+
+	connectionStr := fmt.Sprintf("host=%v port=%v user=%v password=%v dbname=%v sslmode=disable", viper.GetString("db.host"), viper.GetInt("db.port"), viper.GetString("db.username"), viper.GetString("db.password"), viper.GetString("db.database"))
+
+	db, err := sql.Open("postgres", connectionStr)
+	if err != nil {
+		panic(err)
+	}
+
+	db.Close()
+
 	app := fiber.New(fiber.Config{
 		Immutable: true,
 	})
@@ -20,5 +36,16 @@ func main() {
 	auth.Post("/login", authentication.Login)
 	auth.Post("/register", authentication.Register)
 
-	app.Listen(":8080")
+	app.Listen(viper.GetString("app.port"))
+}
+
+func initConfig() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("fatal error config file: %w", err))
+	}
 }
